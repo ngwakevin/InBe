63b4af63c16ee06270afd0b6a0e95f2ae3d7d3a7
ngwa kevin <kevin.ngwa@kemin.com>
2025-09-14 21:27:48 +0200

    feat(auth): point authority to new user flow B2X_1_signup_signin2

--- js/auth-config.js
+++ js/auth-config.js
@@ -5,7 +5,7 @@
 const msalConfig = {
   auth: {
     clientId: '5a3dbeb5-adb1-4291-b91c-9e5d4169f7c4',
-    authority: 'https://ngwakevinoutlook.b2clogin.com/ngwakevinoutlook.onmicrosoft.com/B2X_1_signup_signin1',
+  authority: 'https://ngwakevinoutlook.b2clogin.com/ngwakevinoutlook.onmicrosoft.com/B2X_1_signup_signin2',
     knownAuthorities: ['ngwakevinoutlook.b2clogin.com'],
     redirectUri: window.location.origin + '/index.html'
   },
